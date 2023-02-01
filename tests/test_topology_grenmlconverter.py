753aee628b896d009069967aeb9801d6c585e125
Sajith Sasidharan <sajith@hcoop.net>
2023-02-01 13:57:04 -0600

    Refactor GrenmlConverter tests

--- tests/test_topology_grenmlconverter.py
+++ tests/test_topology_grenmlconverter.py
@@ -6,38 +6,36 @@ from sdx.pce.topology.grenmlconverter import GrenmlConverter
 
 
 class GrenmlConverterTests(unittest.TestCase):
+    """
+    Tests for GrenmlConverter.
+    """
 
     TEST_DATA_DIR = pathlib.Path(__file__).parent.joinpath("data")
     AMLIGHT_TOPOLOGY_FILE = TEST_DATA_DIR.joinpath("amlight.json")
-    
+
     def setUp(self):
         pass
-    
+
     def tearDown(self):
         pass
 
-    def testGrenmlConverter(self):
+    def test_grenml_converter_amlight(self):
         manager = TopologyManager()
 
         # TODO: this does not raise errors when it should (such as
         # when the input file is not present). Make the necessary
         # change in datamodel's TopologyHandler class.
         manager.handler.topology_file_name(self.AMLIGHT_TOPOLOGY_FILE)
-        topology = manager.handler.import_topology()
-        
-        try:
-            print("Test Topology Converter")
-            print(f"Topology: {topology}")
+        manager.handler.import_topology()
 
-            converter = GrenmlConverter(topology)
-            self.assertIsNotNone(converter)
+        print(f"Topology: {manager.handler.topology}")
+        self.assertIsNotNone(manager.handler.topology, "No topology could be read")
 
-            converter.read_topology()
-            xml = converter.get_xml_str()
-            print(f"XML: {xml}")
-            self.assertIsNotNone(xml)
-            
-        except Exception as e:
-            print(e)
-            return False
-        return True
+        converter = GrenmlConverter(manager.handler.topology)
+        print(f"GrenmlConverter: {converter}")
+        self.assertIsNotNone(converter, "Could not create GRENML converter")
+
+        converter.read_topology()
+        xml = converter.get_xml_str()
+        print(f"XML: {xml}")
+        self.assertIsNotNone(xml)
