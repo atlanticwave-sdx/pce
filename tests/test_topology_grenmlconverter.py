692228a48f8f7de38f7b9741c9ff30d048f40f44
Sajith Sasidharan <sajith@hcoop.net>
2023-02-01 13:47:59 -0600

    Refactor test

--- tests/test_topology_grenmlconverter.py
+++ tests/test_topology_grenmlconverter.py
@@ -7,23 +7,35 @@ from sdx.pce.topology.grenmlconverter import GrenmlConverter
 TOPOLOGY_AMLIGHT = "./tests/data/amlight.json"
 
 
-class TestTopologyGRENMLConverter(unittest.TestCase):
+class GrenmlConverterTests(unittest.TestCase):
+    
     def setUp(self):
-        self.manager = TopologyManager()  # noqa: E501
-        self.handler = self.manager.handler
-        self.handler.topology_file_name(TOPOLOGY_AMLIGHT)
-        self.handler.import_topology()
-
+        pass
+    
     def tearDown(self):
         pass
 
     def testGrenmlConverter(self):
+        manager = TopologyManager()
+
+        # TODO: this does not raise errors when it should (such as
+        # when the input file is not present). Make the necessary
+        # change in datamodel's TopologyHandler class.
+        manager.handler.topology_file_name(TOPOLOGY_AMLIGHT)
+        topology = manager.handler.import_topology()
+        
         try:
             print("Test Topology Converter")
-            print(self.handler.topology)
-            converter = GrenmlConverter(self.handler.topology)
+            print(f"Topology: {topology}")
+
+            converter = GrenmlConverter(topology)
+            self.assertIsNotNone(converter)
+
             converter.read_topology()
-            print(converter.get_xml_str())
+            xml = converter.get_xml_str()
+            print(f"XML: {xml}")
+            self.assertIsNotNone(xml)
+            
         except Exception as e:
             print(e)
             return False
