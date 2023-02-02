969464e019047c982a5eb2d0f1d258bfa4533576
Sajith Sasidharan <sajith@hcoop.net>
2023-02-02 08:34:06 -0600

    Refactor TEManager test

--- tests/test_te_manager.py
+++ tests/test_te_manager.py
@@ -4,37 +4,36 @@ import unittest
 
 from sdx.pce.topology.temanager import TEManager
 
-TEST_DATA_DIR = pathlib.Path(__file__).parent.joinpath("data")
-TOPOLOGY_FILE = TEST_DATA_DIR.joinpath("sdx.json")
-CONNECTION_REQ_FILE = TEST_DATA_DIR.joinpath("test_request.json")
 
-
-class TestTopologyManager(unittest.TestCase):
+class TestTEManager(unittest.TestCase):
     """
     Tests for topology related functions.
     """
 
+    TEST_DATA_DIR = pathlib.Path(__file__).parent.joinpath("data")
+    TOPOLOGY_FILE = TEST_DATA_DIR.joinpath("sdx.json")
+    CONNECTION_REQ_FILE = TEST_DATA_DIR.joinpath("test_request.json")
+
     def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_generate_solver_input(self):
-        with open(TOPOLOGY_FILE, "r", encoding="utf-8") as fp:
+        with open(self.TOPOLOGY_FILE, "r", encoding="utf-8") as fp:
             topology_data = json.load(fp)
 
-        with open(CONNECTION_REQ_FILE, "r", encoding="utf-8") as fp:
+        with open(self.CONNECTION_REQ_FILE, "r", encoding="utf-8") as fp:
             connection_data = json.load(fp)
 
-        temanager = TEManager(topology_data, connection_data)
+        self.temanager = TEManager(topology_data, connection_data)
 
-        graph = temanager.graph
+    def _make_connection(self):
+        graph = self.temanager.graph
         print(f"Generated networkx graph of the topology: {graph}")
         print(f"Graph nodes: {graph.nodes[0]}, edges: {graph.edges}")
 
-        print("Test Convert Connection To Topology")
-        connection = temanager.generate_connection_te()
+        connection = self.temanager.generate_connection_te()
         print(f"connection: {connection}")
 
+        return connection
+
+    def test_generate_solver_input(self):
+        print("Test Convert Connection To Topology")
+        connection = self._make_connection()
         self.assertIsNotNone(connection)
