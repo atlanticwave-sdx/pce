02c5d445bd138bf46358f778d2ad8e0fae6552d4
Sajith Sasidharan <sajith@hcoop.net>
2023-05-08 15:05:58 -0500

    Reformat

--- src/sdx/pce/topology/temanager.py
+++ src/sdx/pce/topology/temanager.py
@@ -97,8 +97,10 @@ class TEManager:
         required_bandwidth = self.connection.bandwidth
         required_latency = self.connection.latency
 
-        print(f"Setting required_latency: {required_latency}, "
-              f"required_bandwidth: {required_bandwidth}")
+        print(
+            f"Setting required_latency: {required_latency}, "
+            f"required_bandwidth: {required_bandwidth}"
+        )
 
         request = ConnectionRequest(
             source=ingress_nodes[0],
