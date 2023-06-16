e8c5d8a5d37756a3902e463332870ec47781ffc6
Sajith Sasidharan <sajith@hcoop.net>
2023-06-16 15:45:10 -0500

    Keep some extra assertions around, just in case

--- tests/test_te_manager.py
+++ tests/test_te_manager.py
@@ -462,6 +462,22 @@ class TEManagerTests(unittest.TestCase):
         self.assertIsNotNone(updated_breakdown.uni_z.tag.tag_type)
         self.assertIsNotNone(updated_breakdown.uni_z.interface_id)
 
+        # TODO: alternatively, if we're getting a dict back, maybe use
+        # the assertions below.
+
+        # self.assertIsNotNone(updated_breakdown.get("name"))
+        # self.assertIsNotNone(updated_breakdown.get("dynamic_backup_path"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_a"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_a").get("tag"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_a").get("tag").get("value"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_a").get("tag").get("tag_type"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_a").get("interface_id"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_z"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_z").get("tag"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_z").get("tag").get("value"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_z").get("tag").get("tag_type"))
+        # self.assertIsNotNone(updated_breakdown.get("uni_z").get("interface_id"))
+
     def test_generate_graph_and_connection(self):
         graph = self.temanager.generate_graph_te()
         tm = self.temanager.generate_connection_te()
