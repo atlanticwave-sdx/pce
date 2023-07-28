d137507e1000e2e9c0f2238ed97b05ee80bb1e81
Sajith Sasidharan <sajith@hcoop.net>
2023-07-28 12:24:50 -0500

    Use packaged topology and test request files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,20 +1,24 @@
 import pathlib
+from importlib.resources import files
 
 
 class TestData:
+    # Some data files are in src/sdx/pce/data.
+    PACKAGE_DATA_DIR = files("sdx.pce.data")
+
+    TOPOLOGY_FILE_ZAOXI = PACKAGE_DATA_DIR / "topologies" / "zaoxi.json"
+    TOPOLOGY_FILE_SAX = PACKAGE_DATA_DIR / "topologies" / "sax.json"
+    TOPOLOGY_FILE_AMLIGHT = PACKAGE_DATA_DIR / "topologies" / "amlight.json"
+    CONNECTION_REQ = PACKAGE_DATA_DIR / "requests" / "test_request.json"
+
     TEST_DATA_DIR = pathlib.Path(__file__).parent / "data"
 
     TOPOLOGY_DATA_DIR = TEST_DATA_DIR / "topologies"
 
     TOPOLOGY_FILE_SDX = TEST_DATA_DIR / "sdx.json"
 
-    TOPOLOGY_FILE_ZAOXI = TOPOLOGY_DATA_DIR / "zaoxi.json"
-    TOPOLOGY_FILE_SAX = TOPOLOGY_DATA_DIR / "sax.json"
-    TOPOLOGY_FILE_AMLIGHT = TOPOLOGY_DATA_DIR / "amlight.json"
-
     TOPOLOGY_FILE_AMLIGHT_IMG = TOPOLOGY_DATA_DIR / "amlight.png"
 
-    CONNECTION_REQ = TEST_DATA_DIR / "test_request.json"
     CONNECTION_REQ_AMLIGHT = TEST_DATA_DIR / "test_request_amlight.json"
 
     TOPOLOGY_FILE_SAX_2 = TEST_DATA_DIR / "sax-2.json"
