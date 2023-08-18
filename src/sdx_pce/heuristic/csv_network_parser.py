296dc084927d8c632f7ede8f37e37734424ba35f
Yufeng Xin <yxin@renci.org>
2023-08-18 16:36:57 -0400

    save a set of code for tunnel based TE and resiliency optimization, PR will be done seperately with more study and cleanup.

---
+++ src/sdx_pce/heuristic/csv_network_parser.py
@@ -0,0 +1,65 @@
+import random
+from network_topology import *
+import csv
+
+def parse_topology(network_name):
+    network = Network(network_name)
+    with open(network_name) as fi:
+        reader = csv.reader(fi, delimiter=" ")
+        for row_ in reader:
+            if row_[0] == 'to_node': continue
+            row = [x for x in row_ if x]
+            to_node = row[0]
+            from_node = row[1]
+            capacity = int(float(row[2])/1000.0)
+            network.add_node(to_node, None, None)
+            network.add_node(from_node, None, None)
+            network.add_edge(from_node, to_node, 200, capacity)            
+    return network
+
+def parse_demands(network, demand_file,scale=1):
+    num_nodes = len(network.nodes)
+    demand_matrix = {}
+    with open(demand_file) as fi:
+        reader = csv.reader(fi, delimiter=" ")
+        for row_ in reader:
+            if row_[0] == 'to_node': continue
+            row = [float(x) for x in row_ if x]
+            assert len(row) == num_nodes ** 2
+            for idx, dem in enumerate(row):
+                from_node = int(idx/num_nodes) + 1
+                to_node = idx % num_nodes + 1
+                assert str(from_node) in network.nodes
+                assert str(to_node) in network.nodes
+                if from_node not in demand_matrix:
+                    demand_matrix[from_node] = {}
+                if to_node not in demand_matrix[from_node]:
+                    demand_matrix[from_node][to_node] = []
+                demand_matrix[from_node][to_node].append(dem/1000.0)
+        for from_node in demand_matrix:
+            for to_node in demand_matrix[from_node]:
+                max_demand = max(demand_matrix[from_node][to_node])
+                network.add_demand(str(from_node), str(to_node), max_demand, scale)
+    if network.tunnels:
+        remove_demands_without_tunnels(network)
+
+def parse_tunnels(network):
+    # Parse tunnels
+    for node1 in network.nodes:
+        for node2 in network.nodes:
+            if node1 == node2: continue
+            paths = network.k_shortest_paths(node1, node2, 5)
+            for path in paths:
+                tunnel = network.add_tunnel(path)
+    if network.demands:
+        remove_demands_without_tunnels(network)
+
+def remove_demands_without_tunnels(network):
+    removable_demands = [p for p, d in network.demands.items() if not d.tunnels]
+    for demand_pair in removable_demands:
+        del network.demands[demand_pair]
+
+def initialize_weights(network):
+    for tunnel in network.tunnels.values():
+        tunnel.add_weight(random.randint(1, 10))
+        
---
+++ src/sdx_pce/heuristic/path_te_solver.py
@@ -0,0 +1,223 @@
+import copy
+from dataclasses import dataclass
+from itertools import chain, cycle
+from typing import List, Mapping, Tuple, Union
+
+import argparse
+import csv
+import networkx as nx
+import numpy as np
+from ortools.linear_solver import pywraplp
+
+from network_topology import *
+from csv_network_parser import *
+from MIPSolver import GORSolver
+
+from sdx_pce.load_balancing.te_solver import TESolver, DataModel
+
+class PathTESolver:
+    """
+    Path Based Traffic Engineering Solver.
+    """
+
+    def __init__(    
+        self,
+        network:Network,
+    ):
+        """
+        :param graph: A NetworkX graph that represents a network
+            topology.
+        :param tm: Traffic matrix, in the form of a list of connection
+            requests.
+        :param cost_flag: Cost (weight) to assign per link.
+        :param objective: What to solve for: cost or load balancing.
+        """
+        self.network=network
+        #random weight of tunnels
+        initialize_weights(network)
+        # Create the mip solver with the SCIP backend.
+        self.solver = GORSolver()
+        self.initialize_tunnel_variables()
+
+    def create_data_model(self, latency=True) -> DataModel:
+        """
+        Top function to create the OR optimization model in the array format
+        """
+        nodenum = len(self.network.nodes)
+        linknum = len(self.network.edges)
+
+
+        edges=self.network.edges.values()
+        print(f"Creating data model: #nodes: {nodenum}, #links: {linknum}")
+
+        self._add_demand_constraints()
+        self._add_edge_capacity_constraints()
+
+    
+    def _add_demand_constraints(self):
+        """lhs: [L, D], rhs: [D]"""
+
+        tunnels=self.network.tunnels.values()
+        demands=self.network.demands.values()
+        tunnelnum = len(tunnels)
+        demandnum = len(demands)
+        print(f"#tunnels: {tunnelnum}, #demands: {demandnum}")
+
+        for demand in demands:
+            assert len(demand.tunnels) > 0
+            constraint = self.solver.solver.RowConstraint(0, demand.amount, "")
+            for tunnel in demand.tunnels:
+                constraint.SetCoefficient(tunnel.v_flow, 1.0)
+
+    def _add_edge_capacity_constraints(self):
+        """lhs: [L, E], rhs: [E]"""
+        for edge_pair in self.network.edges:
+            edge = self.network.edges[edge_pair]
+            constraint = self.solver.solver.RowConstraint(0, edge.capacity, "")
+            for tunnel in edge.tunnels:
+                constraint.SetCoefficient(tunnel.v_flow, 1.0)
+        print(f"Edge Constraints:{len(self.solver.solver.constraints())}")
+                    
+    def initialize_tunnel_variables(self):
+        for tunnel in self.network.tunnels.values():
+            tunnel.init_flow_var(self.solver)
+
+    def Maximize(self):
+        objective = self.solver.solver.Objective()
+        for tunnel in self.network.tunnels.values():
+            objective.SetCoefficient(tunnel.v_flow, 1)
+        objective.SetMaximization()
+        self.solver.Maximize(objective)
+
+    def Maximize_FCC(self):
+        objective = self.solver.solver.Objective()
+        for demand in self.network.demands.values():
+            objective.SetCoefficient(demand.b_d, 1)
+        objective.SetMaximization()
+        self.solver.Maximize(objective)
+
+    def solve(self):
+        print("Path TE Solver!")
+        print(f"NumVariables:{len( self.solver.solver.variables())}")
+        print(f"NumConstraints:{len( self.solver.solver.constraints())}")
+        status = self.solver.Solve()
+        result=-1.0
+        ordered_paths = {} 
+        if status == pywraplp.Solver.OPTIMAL:
+            result=self.solver.solver.Objective().Value()
+            ordered_paths=self.get_demand_flow_allocation()
+            edge_load=self.get_edge_flow_allocations()
+
+        else:
+            print("The problem does not have an optimal solution.")
+
+        return ordered_paths, result
+
+    def get_demand_flow_allocation(self):
+        ordered_paths={}
+        for demand in self.network.demands.values():
+            src=demand.src
+            dst=demand.dst
+            ordered_paths[(src,dst)]=demand.tunnels
+
+        return ordered_paths
+
+    def criticality(self):
+        pass
+
+    def get_edge_flow_allocations(self):
+        """ 
+        Get the optimal edge allocations.
+        """
+        flow_labels = {}
+        for edge in self.network.edges.values():
+            allocation = 0
+            for tunnel in edge.tunnels:
+                allocation += tunnel.v_flow.SolutionValue()
+            utility=allocation/edge.capacity
+            flow_labels[edge] = (round(allocation, 2), utility)
+
+        return flow_labels
+
+    def get_demands_met(self):
+        demands_met = {}
+        for demand in self.network.demands.values():
+            #print(f"d_b:{demand.b_d.SolutionValue()}")
+            flow_on_tunnels = sum([tunnel.v_flow.SolutionValue() for tunnel in demand.tunnels])
+            demands_met[(demand.src, demand.dst)] = flow_on_tunnels
+        return demands_met
+
+    def get_demands_unmet(self):
+        demands_unmet = {}
+        for demand in self.network.demands.values():
+            flow_on_tunnels = sum([tunnel.v_flow.SolutionValue() for tunnel in demand.tunnels])
+            if demand.amount - flow_on_tunnels > 0:
+                demands_unmet[(demand.src, demand.dst)] = round(demand.amount - flow_on_tunnels)
+        return demands_unmet
+
+
+class FCCPathTESolver(PathTESolver):
+    """
+    Path Based Traffic Engineering Solver with FCC resiliency
+    """
+    def __init__(    
+        self,
+        network:Network,
+    ):
+        PathTESolver.__init__(self, network)
+            
+        for demand in self.network.demands.values():
+            demand.init_b_d(self.solver)
+
+    
+    def create_data_model(self, latency=True) -> DataModel:
+        """
+        Top function to create the OR optimization model in the array format
+        """
+        nodenum = len(self.network.nodes)
+        linknum = len(self.network.edges)
+
+        edges=self.network.edges.values()
+        print(f"Creating data model: #nodes: {nodenum}, #links: {linknum}")
+
+        print(f"NumVariables:{len(self.solver.solver.variables())}")
+        self._add_demand_constraints()
+        self._add_edge_capacity_constraints()
+        
+    def failure_scenario_edge_constraint(self, alpha):
+
+        def tunnel_alpha(tunnel):
+            return 0 if any(set(alpha) & set(tunnel.path)) else 1
+
+        for demand in self.network.demands.values():
+            #constraint = self.solver.solver.RowConstraint(0, demand.amount, "{alpha}")
+            #constraint.SetCoefficient(demand.b_d, 1.0)
+            c=[]
+            for tunnel in demand.tunnels:
+                #constraint.SetCoefficient(tunnel.v_flow, -1.0*tunnel_alpha(tunnel))
+                c.append(tunnel.v_flow*tunnel_alpha(tunnel))
+            self.solver.solver.Add(demand.b_d <= sum(c))
+            
+                            
+    def _add_demand_constraints(self):
+        """lhs: [L, D], rhs: [D]"""
+
+        tunnels=self.network.tunnels.values()
+        demands=self.network.demands.values()
+        tunnelnum = len(tunnels)
+        demandnum = len(demands)
+        print(f"#tunnels: {tunnelnum}, #demands: {demandnum}")
+
+        for demand in demands:
+            constraint = self.solver.solver.RowConstraint(0, demand.amount, "")
+            constraint.SetCoefficient(demand.b_d, 1.0)
+
+        print(f"Demand Constraints:{len(self.solver.solver.constraints())}")
+            
+    def pairwise_failures(self):
+        return itertools.combinations(self.network.edges.values(), r = 2)
+
+class CRIPathTESolver(PathTESolver):
+    """
+    Path Based Traffic Engineering Solver with FCC Criticality
+    """
