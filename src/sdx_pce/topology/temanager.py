e8a0cd7bbc9cb5e8f0545408564838d8ac8b1b63
Italo Valcy <italovalcy@gmail.com>
2025-04-25 13:25:40 -0300

    fix formatting

--- src/sdx_pce/topology/temanager.py
+++ src/sdx_pce/topology/temanager.py
@@ -830,7 +830,9 @@ class TEManager:
                 connection_request["egress_port"]["id"],
             )
             self._logger.info(f"same_domain_user_port_flag: {same_domain_port_flag}")
-            max_number_oxps = connection_request.get("max_number_oxps") or max_number_oxps
+            max_number_oxps = (
+                connection_request.get("max_number_oxps") or max_number_oxps
+            )
 
         # Now generate the breakdown with potential user specified tags
         ingress_user_port = None
